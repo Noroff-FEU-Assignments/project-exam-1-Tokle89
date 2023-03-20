c4f956257b1a9c0d5638f9fea25f661098b759e7
Tokle89 <fredriktokle@gmail.com>
2023-03-20 19:34:14 +0100

    Update postsSlider.js
    made a function to chekc the card with, every time the user clicks the next button

--- js/functions/postsSlider.js
+++ js/functions/postsSlider.js
@@ -5,19 +5,24 @@ export function slider() {
   const rightBtn = document.querySelector(".right_circle-btn");
   const cards = document.querySelectorAll(".card");
   const firstCard = cards[0];
-  let firstImgWidth = firstCard.clientWidth + 50;
+
+  const checkWidth = () => {
+    let firstImgWidth = firstCard.clientWidth + 50;
+    if (leftBtn) {
+      cardsContainer.scrollLeft -= firstImgWidth;
+    }
+    if (rightBtn) {
+      cardsContainer.scrollLeft += firstImgWidth;
+    }
+  };
 
   leftBtn.addEventListener("click", function () {
-    cardsContainer.scrollLeft -= firstImgWidth;
+    checkWidth(leftBtn);
   });
 
   rightBtn.addEventListener("click", function () {
-    cardsContainer.scrollLeft += firstImgWidth;
+    checkWidth(rightBtn);
   });
-
-  // function toggleBtn() {
-  //   console.log(cardsContainer.scrollWidth);
-  // }
 }
 export function viewMore() {
   const btn = document.querySelector(".latest-posts_btn");
