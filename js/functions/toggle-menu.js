6d132726f1b634ee7945364f32c4a9ba0f80ef3f
Tokle89 <fredriktokle@gmail.com>
2023-04-24 19:39:26 +0200

    fixed toggle menu animation

--- js/functions/toggle-menu.js
+++ js/functions/toggle-menu.js
@@ -5,10 +5,8 @@ export function toggleMenu() {
   hamburger.onclick = function () {
     if (nav.classList.contains("show-menu")) {
       nav.classList.remove("show-menu");
-      nav.style.display = "none";
     } else {
       nav.classList.add("show-menu");
-      nav.style.display = "block";
     }
   };
 }
